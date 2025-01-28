8c9e7c8ec8835e6536e3c49e927597ed3ec02f17
wesamustafa <wesamustafa@gmail.com>
2025-01-28 23:45:07 +0200

    strcture directive NgFor and NgIf using before angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './DUMMY_USERS';
 import { TasksComponent } from "./tasks/tasks.component";
+import { NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
