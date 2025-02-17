8b90f7481d1257487bee5f0fc5cfd6e7213f7bd9
wesamustafa <wesamustafa@gmail.com>
2025-02-17 21:13:43 +0200

    create component new task and add condition

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { DUMMY_TASKS } from '../DUMMY_TASKS';
+import { NewTaskComponent } from "./new-task/new-task.component";
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -12,11 +13,14 @@ export class TasksComponent {
   userTasks = DUMMY_TASKS
   @Input({required:true}) userId!:string
   @Input({ required: true }) name!: string
-  
+  isAddTask = false;
   get selectedUserTasks() {
     return this.userTasks.filter((task)=> task.userId === this.userId)
   }
   onCompleteTask(id:string) {
     this.userTasks= this.userTasks.filter((userTask) => userTask.id !== id);
+  }
+  onStartAddTask() {
+    this.isAddTask = true;
   }
 }
