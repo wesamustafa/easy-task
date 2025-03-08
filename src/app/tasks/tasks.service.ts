bd3add3a98230d4dd2bc5f2099a92b9761d7d5d8
wesamustafa <84191799+wesamustafa@users.noreply.github.com>
2025-03-08 23:52:05 +0200

    use local storge

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -6,6 +6,12 @@ import { NewTask } from "./task/task.model";
 })
 export class TaskService{
     private tasks = DUMMY_TASKS
+    constructor(){
+        const tasks = localStorage.getItem('tasks');
+        if(tasks){
+            this.tasks = JSON.parse(tasks);
+        }
+    }
     getUserTasks(userId:string){
         return this.tasks.filter((task)=> task.userId === userId)
     }
@@ -17,8 +23,13 @@ export class TaskService{
             summary:taskData.summary,
             dueDate:taskData.date
           })
+          this.saveTasks();
     }
     removeTask(id:string){
         this.tasks= this.tasks.filter((task) => task.id !== id);
+        this.saveTasks();
+    }
+    private saveTasks(){
+        localStorage.setItem('tasks',JSON.stringify(this.tasks))
     }
 }
