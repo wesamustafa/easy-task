8901467ef2b1e1b6b7343f93b8157fa8283aa47f
wesamustafa <84191799+wesamustafa@users.noreply.github.com>
2025-03-10 00:23:30 +0200

    use angular module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './DUMMY_USERS';
-import { TasksComponent } from "./tasks/tasks.component";
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
